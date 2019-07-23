413207ba7c66a4a07b3a2acbeba8208676c19786
anjali patel <anjali@gmail.com>
2019-07-23 16:31:07 +0530

    Initial commit.

---
+++ src/main/java/com/amdocs/amdocshellorestapi/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.amdocs.amdocshellorestapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloRestController {
+
+	@GetMapping("Hello")
+	public String sayHello() {
+		return " Hello Rest Api";
+	}
+}
